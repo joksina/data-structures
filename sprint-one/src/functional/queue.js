f8e8b790afcb25715c969efb2e4fc09d1aebc32d
joksina <jksalaudeen@yahoo.com>
2015-11-18 14:45:51 -0800

    stack and queues

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var increment = 0;
+  var decrement = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[increment] = value;
+    increment++;
   };
 
   someInstance.dequeue = function() {
+    var diff = increment - decrement;
+    if(diff > 0){
+      var value = storage[decrement];
+      delete storage[decrement];
+      decrement++;
+      return value;
+    }
   };
 
   someInstance.size = function() {
+    return increment - decrement;
   };
 
   return someInstance;
