ddb0fbabe7adea37dbe98c6223514b47c4191200
joksina <jksalaudeen@yahoo.com>
2015-11-19 10:31:18 -0800

    all tests pass for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var result = Node(value);
+    if(list.head === null){
+      list.head = result;
+      list.tail = result;
+    } else {
+      list.head.next = result;
+      list.tail = result;
+    }
   };
 
   list.removeHead = function() {
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while ( current ){
+      if(current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
   };
 
   return list;
