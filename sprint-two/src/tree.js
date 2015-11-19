c35167b0399bb2a9f089dc790b2fa4ec513aa87d
joksina <jksalaudeen@yahoo.com>
2015-11-19 10:59:59 -0800

    all tests passing for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,38 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  // your code here 
+  var child = Tree(value);
+  child.value = value;
+  this.children.push(child);  // fix me
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  var subroutine = function(node){
+    if (node.value === target) {
+    console.log(node.value ===target);
+      result = true;
+    } 
+
+    if(node.children) {
+      _.each(node.children, function(value){
+        subroutine(value);
+      });
+    }
+  };
+
+  subroutine(this);
+  return result;
+
 };
 
 
