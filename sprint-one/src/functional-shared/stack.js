23044dbac1f47711e0cf2cf6fee2996a15b2c0d1
joksina <jksalaudeen@yahoo.com>
2015-11-18 16:18:06 -0800

    all tests passing for prototypal stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,12 +1,12 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var store = {
+  var someInstance = {
     storage: {}
   };
-  _.extend(store, stackMethods);
+  _.extend(someInstance, stackMethods);
 
-  return store;
+  return someInstance;
 };
   var stackMethods = { 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var instance = Object.create(stackMethods);
+instance.storage = {};
+instance.stackSize = 0;
+
+return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  // body...
+  this.storage[this.stackSize] = value;
+  this.stackSize++;
+};
 
+stackMethods.pop = function(){
+  var item = this.storage[this.stackSize - 1];
+  delete this.storage[this.stackSize - 1];
+  this.stackSize--;
+  return item;
+};
+
+stackMethods.size = function(){
+  return this.stackSize < 0 ? 0 : this.stackSize;
+};
